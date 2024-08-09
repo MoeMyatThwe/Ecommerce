58f9473a129d08515f4114e38ca125cbd6a0780c
Moe Myat Thwe <moemyatthwebts2003@gmail.com>
2024-08-09 19:52:18 +0800

    saleOrder example1 finish

--- controllers/saleOrdersController.js
+++ controllers/saleOrdersController.js
@@ -1,3 +1,4 @@
+// --------------------modified code --------------------
 const { EMPTY_RESULT_ERROR, UNIQUE_VIOLATION_ERROR, DUPLICATE_TABLE_ERROR } = require('../errors');
 const saleOrdersModel = require('../models/saleOrders');
 const membersModel = require('../models/members');
@@ -12,7 +13,23 @@ module.exports.retrieveAll = function (req, res) {
                 memberId = null;
             }
 
-            return saleOrdersModel.retrieveAll(memberId);
+            // Get filters from request query
+            const filters = {
+                status: req.query.status,
+                minOrderDatetime: req.query.minOrderDatetime,
+                maxOrderDatetime: req.query.maxOrderDatetime,
+                minItemQuantity: req.query.minItemQuantity,
+                maxItemQuantity: req.query.maxItemQuantity,
+                productDescription: req.query.productDescription,
+                minUnitPrice: req.query.minUnitPrice,
+                maxUnitPrice: req.query.maxUnitPrice,
+                memberUsername: req.query.memberUsername,
+                minMemberDob: req.query.minMemberDob,
+                maxMemberDob: req.query.maxMemberDob,
+                sortOrder: req.query.sortOrder,
+            };
+
+            return saleOrdersModel.retrieveAll(memberId, filters);
         })
         .then(function (saleOrders) {
             return res.json({ saleOrders: saleOrders });
@@ -24,5 +41,39 @@ module.exports.retrieveAll = function (req, res) {
             }
             return res.status(500).json({ error: error.message });
         });
+};
 
-}
+
+            
+// ------------------------------------------
+
+// ---------Original code--------------------------
+// const { EMPTY_RESULT_ERROR, UNIQUE_VIOLATION_ERROR, DUPLICATE_TABLE_ERROR } = require('../errors');
+// const saleOrdersModel = require('../models/saleOrders');
+// const membersModel = require('../models/members');
+
+// module.exports.retrieveAll = function (req, res) {
+//     let memberId = res.locals.member_id;
+
+//     membersModel
+//         .isAdmin(memberId)
+//         .then(function (isAdmin) {
+//             if (isAdmin) {
+//                 memberId = null;
+//             }
+
+//             return saleOrdersModel.retrieveAll(memberId);
+//         })
+//         .then(function (saleOrders) {
+//             return res.json({ saleOrders: saleOrders });
+//         })
+//         .catch(function (error) {
+//             console.error(error);
+//             if (error instanceof EMPTY_RESULT_ERROR) {
+//                 return res.status(404).json({ error: error.message });
+//             }
+//             return res.status(500).json({ error: error.message });
+//         });
+
+// }
+// -----------------------------
--- models/saleOrders.js
+++ models/saleOrders.js
@@ -1,21 +1,117 @@
-const { query } = require('../database');
-const { EMPTY_RESULT_ERROR, SQL_ERROR_CODE, UNIQUE_VIOLATION_ERROR } = require('../errors');
+// const { query } = require('../database');
+// const { EMPTY_RESULT_ERROR, SQL_ERROR_CODE, UNIQUE_VIOLATION_ERROR } = require('../errors');
 
-module.exports.retrieveAll = function retrieveAll(memberId) {
-    let params = [];
-    let sql = `SELECT * FROM sale_order_item s JOIN sale_order o ON s.sale_order_id=o.id JOIN product p ON s.product_id=p.id JOIN member m ON o.member_id=m.id`;
+// module.exports.retrieveAll = function retrieveAll(memberId) {
+//     let params = [];
+//     let sql = `SELECT * FROM sale_order_item s JOIN sale_order o ON s.sale_order_id=o.id JOIN product p ON s.product_id=p.id JOIN member m ON o.member_id=m.id`;
+//     if (memberId) {
+//         sql += ` WHERE o.member_id = $1`
+//         params.push(memberId);
+//     }
+//     return query(sql, params).then(function (result) {
+//         const rows = result.rows;
+
+//         if (rows.length === 0) {
+//             throw new EMPTY_RESULT_ERROR(`Sale Order not found!`);
+//         }
+
+//         return rows;
+//     });
+// };
+
+// ---------------------------------modified code--------------
+const { PrismaClient } = require('@prisma/client');
+const { EMPTY_RESULT_ERROR } = require('../errors');
+
+const prisma = new PrismaClient();
+
+module.exports.retrieveAll = function retrieveAll(memberId, filters = {}) {
+    const {
+        status,
+        minOrderDatetime,
+        maxOrderDatetime,
+        minItemQuantity,
+        maxItemQuantity,
+        productDescription,
+        minUnitPrice,
+        maxUnitPrice,
+        memberUsername,
+        minMemberDob,
+        maxMemberDob,
+        sortOrder
+    } = filters;
+
+    // Build the query using Prisma
+    const whereClause = {};
+
     if (memberId) {
-        sql += ` WHERE o.member_id = $1`
-        params.push(memberId);
+        whereClause.memberId = memberId;
     }
-    return query(sql, params).then(function (result) {
-        const rows = result.rows;
 
-        if (rows.length === 0) {
+    if (status) {
+        whereClause.status = { in: status.split(',') };
+    }
+
+    if (minOrderDatetime) {
+        whereClause.orderDatetime = { gte: new Date(minOrderDatetime) };
+    }
+
+    if (maxOrderDatetime) {
+        whereClause.orderDatetime = {
+            ...(whereClause.orderDatetime || {}),
+            lte: new Date(maxOrderDatetime)
+        };
+    }
+
+    if (minItemQuantity || maxItemQuantity || productDescription || minUnitPrice || maxUnitPrice) {
+        whereClause.saleOrderItem = {
+            some: {
+                ...(minItemQuantity && { quantity: { gte: parseInt(minItemQuantity, 10) } }),
+                ...(maxItemQuantity && { quantity: { lte: parseInt(maxItemQuantity, 10) } }),
+                ...(productDescription && {
+                    product: {
+                        description: { contains: productDescription, mode: 'insensitive' }
+                    }
+                }),
+                ...(minUnitPrice && { product: { unitPrice: { gte: parseFloat(minUnitPrice) } } }),
+                ...(maxUnitPrice && { product: { unitPrice: { lte: parseFloat(maxUnitPrice) } } }),
+            }
+        };
+    }
+
+    if (memberUsername || minMemberDob || maxMemberDob) {
+        whereClause.member = {
+            ...(memberUsername && { username: { contains: memberUsername, mode: 'insensitive' } }),
+            ...(minMemberDob && { dob: { gte: new Date(minMemberDob) } }),
+            ...(maxMemberDob && { dob: { lte: new Date(maxMemberDob) } }),
+        };
+    }
+
+    return prisma.saleOrder.findMany({
+        where: whereClause,
+        include: {
+            saleOrderItem: {
+                include: {
+                    product: true
+                }
+            },
+            member: true
+        },
+        orderBy: {
+            orderDatetime: sortOrder === 'Order Datetime - Earliest First' ? 'asc' : 'desc'
+        }
+    })
+    .then(saleOrders => {
+        if (saleOrders.length === 0) {
             throw new EMPTY_RESULT_ERROR(`Sale Order not found!`);
         }
 
-        return rows;
+        return saleOrders;
+    })
+    .catch(error => {
+        console.error('Error retrieving sale orders:', error);
+        throw new Error('Failed to retrieve sale orders');
     });
 };
 
+// --------------------------------------------
